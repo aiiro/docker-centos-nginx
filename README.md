16d8c4b418c400725468786bbbd806fbaf6206c3
aiiro <akairo29@gmail.com>
2016-11-28 09:06:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# docker-centos-nginx
+
+## Usage
+- docker-compose
+```
+$ docker-compose up
+$ docker-compose up -d
+```
+
+- docker run
+```
+$ docker run -d --name=web -p=80:80 -p=443:443 aiiro/centos-nginx:latest
+```
+
+
---
+++ container-files/config/init/nginx-init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+# create document root directory and index.html
+mkdir -p $DEFAULT_DOCUMENT_ROOT
+
+mkdir -p /data/conf/nginx/conf.d
+mkdir -p /data/conf/nginx/hosts.d
+mkdir -p /data/conf/nginx/nginx.d
+chmod 711 /data/conf/nginx
+
+mkdir -p /data/tmp/nginx/client_temp
+mkdir -p /data/tmp/nginx/proxy_temp
+mkdir -p /data/tmp/nginx/fastcgi_cache
+mkdir -p /data/tmp/nginx/fastcgi_cache_tmp
+chmod 711 /data/tmp/nginx
+
+mkdir -p /data/www
+chown -R www:www /data/www
